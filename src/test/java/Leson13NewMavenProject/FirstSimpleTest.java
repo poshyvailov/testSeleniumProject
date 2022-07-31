17de3fee60470cf63f6b63056fef5c6256720f6c
aleksandrposhyvailov <pelmel12@gmail.com>
2022-07-31 17:35:48 +0300

    Added first simple Test

---
+++ src/test/java/Leson13NewMavenProject/FirstSimpleTest.java
@@ -0,0 +1,13 @@
+package Leson13NewMavenProject;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FirstSimpleTest {
+
+    @Test
+    public void simpleTest (){
+        Assert.assertTrue(true);
+        Assert.assertEquals(12, 6+6);
+    }
+}
