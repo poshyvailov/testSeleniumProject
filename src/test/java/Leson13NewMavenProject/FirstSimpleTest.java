dc60b0f33bcdc086f042b70bc03b2df76f147256
poshyvailov <pelmel12@gmail.com>
2022-08-02 15:16:31 +0300

    Added Before and After

--- src/test/java/Leson13NewMavenProject/FirstSimpleTest.java
+++ src/test/java/Leson13NewMavenProject/FirstSimpleTest.java
@@ -1,20 +1,30 @@
 package Leson13NewMavenProject;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class FirstSimpleTest {
+    private static WebDriver driver;
 
-    @Test
-    public void setUpDriver(){
+
+    @BeforeAll
+    public static void setUpDriver() {
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://www.google.com/");
-        driver.close();
+        driver = new ChromeDriver();
     }
 
+    @Test
+    public void goToTheMainPage() {
+        driver.get("https://open.spotify.com/");
+        Assertions.assertTrue(driver.getCurrentUrl().contains("spotify.com"));
+    }
+
+    @AfterEach
+    public void closeTheBrowser() {
+        driver.close();
+    }
 
 }
