d914981e47adca3c091ca67c760daddbb192f4b0
poshyvailov <pelmel12@gmail.com>
2022-08-02 10:42:52 +0300

    Added junit jupiter

--- src/test/java/Leson13NewMavenProject/FirstSimpleTest.java
+++ src/test/java/Leson13NewMavenProject/FirstSimpleTest.java
@@ -1,7 +1,6 @@
 package Leson13NewMavenProject;
 
-import org.junit.Assert;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,6 +8,7 @@ public class FirstSimpleTest {
 
     @Test
     public void initDriver(){
+        System.setProperty("webdriver.chrome.driver", "/Users/poshivailov1/Desktop/chromedriver");
         WebDriver driver = new ChromeDriver();
         driver.get("google.com");
     }
